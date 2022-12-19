f35aced58babcb2c964205cdf8d14d334fe8671f
yktt-nuane <tyinnagoyauniversity@gmail.com>
2022-12-19 10:05:30 +0900

    introduce tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <>
       <Center style={{ width: 400, height: 200 }}>
         <Button>Hello</Button>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </Center>
     </>
   )
