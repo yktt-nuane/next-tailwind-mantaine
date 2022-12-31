c56896a167225861d124605f1dbf2ce1ebb53fb3
yktt-nuane <tyinnagoyauniversity@gmail.com>
2022-12-31 13:41:29 +0900

    revise oreview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,7 @@
 import '../src/styles/globals.css'
 
+import * as NextImage from 'next/image'
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -9,3 +11,10 @@ export const parameters = {
     },
   },
 }
+
+const OriginalNextImage = NextImage.default
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+})
