a64b10df3b13b2b214f298480ba58ad16887601f
yktt-nuane <tyinnagoyauniversity@gmail.com>
2022-12-31 13:43:20 +0900

    revise main.js for path

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -10,4 +12,20 @@ module.exports = {
     builder: '@storybook/builder-webpack5',
   },
   staticDirs: ['../public'],
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@/components': path.resolve(__dirname, '../src/components'),
+          '@/lib': path.resolve(__dirname, '../src/lib'),
+          '@/models': path.resolve(__dirname, '../src/models'),
+          '@/pages': path.resolve(__dirname, '../src/pages'),
+          '@/styles': path.resolve(__dirname, '../src/styles'),
+        },
+      },
+    }
+  },
 }
